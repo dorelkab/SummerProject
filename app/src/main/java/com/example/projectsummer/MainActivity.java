c3590a5e917dd0ec1eaa5bd5df17fd70af82e23d
dorelkab <48754168+dorelkab@users.noreply.github.com>
2019-08-19 13:33:52 +0300

    a hey

--- app/src/main/java/com/example/projectsummer/MainActivity.java
+++ app/src/main/java/com/example/projectsummer/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//hey marina
         setContentView(R.layout.activity_main);
     }
 }
