ffce8c513288e17af71b567a2385da55937253fc
dorelkab <48754168+dorelkab@users.noreply.github.com>
2019-08-28 19:58:04 +0300

    added a jar to resources

--- BackEnd/Crawler/src/Spider.java
+++ BackEnd/Crawler/src/Spider.java
@@ -1,2 +1,10 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class Spider {
+    private Set<String> pagesVisited = new HashSet<String>();
+    private List<String> pagesToVisit=new LinkedList<String>();
+
 }
